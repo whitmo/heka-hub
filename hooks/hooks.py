c8214c1b52fbd51cbb0611ae9fb2bd27ea67d1d3
Whit Morriss <whit.morriss@canonical.com>
2015-02-06 15:20:46 -0600

    more sensible default hooking

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python
 from charmhelpers.contrib.ansible import AnsibleHooks
 from charmhelpers.contrib.ansible import install_ansible_support
+from path import path
 import sys
 
 
-hook_names = [
-    'install',
-    'start',
-    'stop',
-    'upgrade-charm',
-    'config-changed',
-    'heka_in-relation-joined',
-    'heka_in-relation-changed',
-    'logstash_out-relation-joined',
-    'logstash_out-relation-changed',
-    'schema_out-relation-joined',
-    'schema_out-relation-changed',
-    'stats_out-relation-joined',
-    'stats_out-relation-changed',
-    'whisper_out-relation-changed'
-]
+def hook_names(here=path(__file__).parent):
+    for name in (x.basename() \
+                 for x in here.files() if x.islink()):
+        yield name
+
 
 hooks = AnsibleHooks(playbook_path='playbook/main.yml',
-                     default_hooks=hook_names)
+                     default_hooks=list(hook_names()))
 
 
 @hooks.hook('install', 'upgrade-charm')
